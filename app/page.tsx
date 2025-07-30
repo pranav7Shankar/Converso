59621bc8b634373749269e915a7565bc06a6d2a9
pranav7Shankar <pranav7shankar@gmail.com>
2025-07-30 23:28:40 +0530

    Campanion Form Complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
 import React from 'react'
-import {Button} from "@/components/ui/button";
+
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className={"text-2xl underline"}>Welcome to my SaaS app</h1>
-        <Button>Let's get Started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+        <section className={"home-section"}>
+            <CompanionCard
+                id={"123"}
+                name={" Neura the Brainy Explorer"}
+                topic={"Neural network of the brain"}
+                subject ={"science"}
+                duration={45}
+                color={"#ffda6e"}
+            />
+            <CompanionCard
+                id={"456"}
+                name={"Countsy the Number Wizard"}
+                topic={"Derivatives and Integrals"}
+                subject ={"maths"}
+                duration={30}
+                color={"#E5D0FF"}
+            />
+            <CompanionCard
+                id={"789"}
+                name={"Verba the Vocabulary Builder"}
+                topic={"language"}
+                subject ={"english literature"}
+                duration={30}
+                color={"#BDE7FF"}
+            />
+        </section>
+        <section className={"home-section"}>
+            <CompanionsList
+                title={"Recently Completed Sessions"}
+                companions={recentSessions}
+                classNames={"w-2/3 max-lg:w-full"}
+            />
+            <CTA/>
+        </section>
+    </main>
   )
 }
 
