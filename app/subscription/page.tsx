8d898ce3b876d0d32326cb2819e6286f4dcfaa8a
pranav7Shankar <pranav7shankar@gmail.com>
2025-07-31 04:06:01 +0530

    Clerk Auth and Billing Complete

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {PricingTable} from "@clerk/nextjs";
 
 const Subscription = () => {
     return (
-        <div>Subscription</div>
+        <div>
+            <p className={"text-2xl text-center font-bold mt-5 "}>
+                Subscription Plans to suit your Learning needs
+            </p>
+            <div className={"mt-5 flex md:mx-4 sm:mx-9 "}>
+            <PricingTable/>
+            </div>
+        </div>
     )
 }
 export default Subscription
